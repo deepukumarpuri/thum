af48211f978656630b0fa0fa30f15f16dc2af202
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-11-21 08:13:45 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,8 +1,8 @@
 
-from os
+import os
 
 
-class Var(object):
+class Config(object):
     API_ID = int(os.environ.get('API_ID')
     API_HASH = os.environ.get('API_HASH')
     BOT_TOKEN = os.environ.get('BOT_TOKEN')
