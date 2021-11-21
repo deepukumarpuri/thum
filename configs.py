ac28b81e67366ba9c4995f2b72886cc966f56dfc
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-11-21 08:20:20 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,12 +1,10 @@
-
 import os
 
 
 class Config(object):
-    API_ID = int(os.environ.get('API_ID')
-    API_HASH = os.environ.get('API_HASH')
-    BOT_TOKEN = os.environ.get('BOT_TOKEN')
-    SESSION_NAME = os.environ.get('SESSION_NAME', 'F2LxBot')
-    BIN_CHANNEL = int(os.environ.get('BIN_CHANNEL')
-    DATABASE_URL = int(os.environ.get('DATABASE_URL')
-    UPDATES_CHANNEL = int(os.environ.get('UPDATES_CHANNEL', None)
+	API_ID = int(os.environ.get("API_ID"))
+	API_HASH = os.environ.get("API_HASH")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN")
+	DATABASE_URL = os.environ.get("DATABASE_URL")
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
+	BIN_CHANNEL = int(os.environ.get("BIN_CHANNEL"))
