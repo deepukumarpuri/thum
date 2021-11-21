31694f2f8dcad9267de94dbe17b6e1ab41207e86
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-11-21 07:36:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
-# Author: Fayas (https://github.com/FayasNoushad) (@FayasNoushad)
-
 import os
-from ytthumb import ytthumb
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
-START_TEXT = """Hello {},
+START_TEXT = """
+Hello {},
 I am a simple youtube thumbnail downloader telegram bot.
-
 - Send a youtube video link or video ID.
 - I will send the thumbnail.
-- You can also send youtube video link or video id with quality. ( like :- `rokGy0huYEA | sd`
-  - sd - Standard Quality
-  - mq - Medium Quality
-  - hq - High Quality
-  - maxres - Maximum Resolution
+Made by @FayasNoushad
+"""
 
-Made by @FayasNoushad"""
-
-BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Join Updates Channel ⚙', url='https://telegram.me/FayasNoushad')]])
+BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('⚙ Join Updates Channel ⚙', url='https://telegram.me/FayasNoushad')
+        ]]
+    )
+REGEX = r"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$"
 
 Bot = Client(
     "YouTube-Thumbnail-Downloader",
@@ -27,7 +24,6 @@ Bot = Client(
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"]
 )
-
 
 @Bot.on_message(filters.private & filters.command(["start"]))
 async def start(bot, update):
@@ -40,7 +36,6 @@ async def start(bot, update):
         quote=True
     )
 
-
 @Bot.on_message(filters.private & filters.text)
 async def send_thumbnail(bot, update):
     message = await update.reply_text(
@@ -48,17 +43,14 @@ async def send_thumbnail(bot, update):
         disable_web_page_preview=True,
         quote=True
     )
+    if ("youtube.com" in update.text) and ("/" in update.text) and ("=" in update.text):
+        id = update.text.split("=")[-1]
+    elif ("youtu.be" in update.text) and ("/" in update.text):
+        id = update.text.split("/")[-1]
+    else:
+        id = update.text
     try:
-        if " | " in update.text:
-            video = update.text.split(" | ", -1)[0]
-            quality = update.text.split(" | ", -1)[1]
-        else:
-            video = update.text
-            quality = "sd"
-        thumbnail = ytthumb.thumbnail(
-            video=video,
-            quality=quality
-        )
+        thumbnail = "https://img.youtube.com/vi/" + id + "/sddefault.jpg"
         await update.reply_photo(
             photo=thumbnail,
             reply_markup=BUTTONS,
@@ -71,6 +63,5 @@ async def send_thumbnail(bot, update):
             disable_web_page_preview=True,
             reply_markup=BUTTONS
         )
-
 
 Bot.run()
