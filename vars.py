880e02e15e0ee3d82a28a3acf6cbc78583d3658f
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-11-21 08:12:35 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,10 +3,10 @@ from os
 
 
 class Var(object):
-    API_ID = int(os.environ.get('API_ID'))
-    API_HASH = os.environ.get('API_HASH'))
-    BOT_TOKEN = os.environ.get('BOT_TOKEN'))
-    SESSION_NAME = os.environ.get('SESSION_NAME', 'F2LxBot'))
-    BIN_CHANNEL = int(os.environ.get('BIN_CHANNEL'))
-    DATABASE_URL = int(os.environ.get('DATABASE_URL'))
-    UPDATES_CHANNEL = int(os.environ.get('UPDATES_CHANNEL', None))
+    API_ID = int(os.environ.get('API_ID')
+    API_HASH = os.environ.get('API_HASH')
+    BOT_TOKEN = os.environ.get('BOT_TOKEN')
+    SESSION_NAME = os.environ.get('SESSION_NAME', 'F2LxBot')
+    BIN_CHANNEL = int(os.environ.get('BIN_CHANNEL')
+    DATABASE_URL = int(os.environ.get('DATABASE_URL')
+    UPDATES_CHANNEL = int(os.environ.get('UPDATES_CHANNEL', None)
